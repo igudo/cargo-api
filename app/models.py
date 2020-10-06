297793568a5121624fc144ae61712d9047ea4d87
Denis Mulin <gudo.m@yandex.ru>
2020-10-06 17:16:50 +0400

    Added models

---
+++ app/models.py
@@ -0,0 +1,19 @@
+from tortoise import fields, models
+from tortoise.contrib.pydantic import pydantic_model_creator
+
+
+class PricePlan(models.Model):
+    """
+    The Price Plan model
+    """
+
+    id = fields.IntField(pk=True)
+    cargo_type = fields.CharField(max_length=30, default="misc")
+    rate = fields.FloatField()
+    active_date = fields.DatetimeField()
+
+
+    class PydanticMeta:
+        computed = ["id"]
+
+PricePlan_Pydantic = pydantic_model_creator(Users, name="User")
